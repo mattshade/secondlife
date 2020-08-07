0065bc24813aa1842117166a5b966a067c804355
mattshade <matt.shade@nbcuni.com>
2020-08-06 22:05:45 -0400

    updates seo titles

--- src/pages/success.js
+++ src/pages/success.js
@@ -6,7 +6,7 @@ import background from "../images/kids-eating.jpg"
 
 const SecondPage = () => (
   <Layout>
-    <SEO title="Page two" />
+    <SEO title="Success" />
     <div className={`indexWrapper`} style={{ backgroundImage: `url(${background})`, top:`0`, left: `0`, zIndex: `0`, width: `100%`, height:`100%`, backgroundPosition: `center center`, backgroundRepeat: `no-repeat`, backgroundAttachment: `local`, backgroundSize: `cover`, position: `fixed`  }}>
 
     <div style={{zIndex: `1`, maxWidth:`300px`, marginBottom: `1.45rem`, marginTop: `7rem`, marginLeft: `auto`, marginRight: `auto`, textAlign: `center` }}>
